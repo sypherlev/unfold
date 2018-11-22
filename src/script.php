cfb7b623e5d64bbb97f3e33babe0eee2c83a481a
Claire Ryan <cryan@pcis.com>
2018-11-22 14:15:01 -0800

    Fixing autoloading

--- src/script.php
+++ src/script.php
@@ -4,5 +4,28 @@ if (php_sapi_name() != "cli") {
     die("Restricted to command line access only.");
 }
 
+$autoload = null;
+
+$autoloadFiles = [
+    __DIR__ . '/../vendor/autoload.php',
+    __DIR__ . '/../../../autoload.php'
+];
+
+foreach ($autoloadFiles as $autoloadFile) {
+    if (file_exists($autoloadFile)) {
+        $autoload = realpath($autoloadFile);
+        break;
+    }
+}
+
+if (! $autoload) {
+    echo "Autoload file not found; try 'composer dump-autoload' first." . PHP_EOL;
+    exit(1);
+}
+
+require $autoload;
+
+array_shift($argv);
+
 $unfold = new \SypherLev\Unfold\Unfold();
 $unfold->build($argv);
